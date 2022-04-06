b5f0d8094e635036e0c3d1c363ae896c7c4f9b24
mathelho <matheus.coelho789@gmail.com>
2022-04-06 14:29:41 -0300

    Desafio concluído

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,47 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    const idTask: number = (Math.random() * 10); // id random
+    
+    const creatingTask: Task = {
+      id: idTask,
+      title: newTaskTitle,
+      isComplete: false
+    }
+
+    console.log(creatingTask);
+
+    if (creatingTask.title != '') { // nao permitir criar caso o título seja vazio!
+      setTasks(tarefa => [...tarefa, creatingTask]);
+      setNewTaskTitle('');
+    } else {
+      alert('Título vazio! Não é possível adicionar a tarefa.');
+    }
+    
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const confirmTask = tasks.map(item => {
+      if (item.id == id) {
+        return { // se encontrar a task com o ID, retorna um novo objeto com um novo isComplete
+          ...item,
+          isComplete: !item.isComplete
+        }
+
+      } else { // caso contrário só retorna o objeto que já está lá
+        return item;
+      }
+    })
+
+    setTasks(confirmTask);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const tasksWithoutRemovedOne = tasks.filter(item => item.id != id);
+
+    setTasks(tasksWithoutRemovedOne);
   }
 
   return (
